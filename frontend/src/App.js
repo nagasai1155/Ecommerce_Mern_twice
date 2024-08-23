ac94b63b40e7458b7fb7fddabdf6886e92dfeacb
nagasai1155 <nagasaibalam@gmail.com>
2024-08-23 22:42:52 +0530

    login page done and also mens , womens ,kids pages are also done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
-
+import men_banner from './Components/Assests/banner_mens.png'
+import women_banner from './Components/Assests/banner_women.png'
+import kid_banner from './Components/Assests/banner_kids.png'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Footer from './Components/Footer/Footer';
 const App = () => {
@@ -16,9 +18,9 @@ const App = () => {
     <Navbar/>
     <Routes>
       <Route path='/' element={<Shop/>}/>
-      <Route path='/mens' element={<ShopCategory></ShopCategory>}/>
-      <Route path='/womens' element={<ShopCategory></ShopCategory>}/>
-      <Route path='/kids' element={<ShopCategory />}/>
+      <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"></ShopCategory>}/>
+      <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"></ShopCategory>}/>
+      <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
       <Route path="/product" element={<Product/>}>
         <Route path=':productId' element={<Product/>}/>
       </Route>
