7e36fb796f2c09c006fa95acf2d2c2f007317e50
nagasai1155 <nagasaibalam@gmail.com>
2024-08-23 22:03:25 +0530

    Newcollections componetnt done with some more datawith images

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
 
 const Shop = () => {
   return (
@@ -9,6 +11,8 @@ const Shop = () => {
 
      <Hero></Hero>
      <Popular></Popular>
+     <Offers></Offers>
+     <NewCollections></NewCollections>
     </div>
   )
 }
