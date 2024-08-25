8f57d05183f214fe5119abf4da8078a8096f620b
nagasai1155 <nagasaibalam@gmail.com>
2024-08-25 21:42:12 +0530

    addproduct ,prodcut list done and addproduct to database is successfully done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Admin from './Pages/Admin/Admin'
+export const backend_url = 'http://localhost:4000';
+export const currency = '₹';
 
 const App = () => {
   return (
