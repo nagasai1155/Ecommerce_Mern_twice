f81aa2fb3baa92f797fbca7dc3eaa015d5898f46
nagasai1155 <nagasaibalam@gmail.com>
2024-08-26 08:46:42 +0530

    done with the signup verification using jwt token in database and checking in thè thunder client

--- backend/index.js
+++ backend/index.js
@@ -118,6 +118,51 @@ app.get('/getproducts',async(req,res)=>{
     console.log("all products fetched");
     res.send(products);
 })
+
+
+
+// Schema for creating user model
+const Users = mongoose.model("Users", {
+    name: { type: String },
+    email: { type: String, unique: true },
+    password: { type: String },
+    cartData: { type: Object },
+    date: { type: Date, default: Date.now() },
+  });
+
+  //Create an endpoint at ip/auth for regestring the user & sending auth-token
+app.post('/signup', async (req, res) => {
+    console.log("Sign Up");
+    let success = false;
+    //finding the user having the existing account or not 
+    let check = await Users.findOne({ email: req.body.email });
+    if (check) {
+      return res.status(400).json({ success: success, errors: "existing user found with this email" });
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+      cart[i] = 0;
+    }
+    const user = new Users({
+      name: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      cartData: cart,
+    });
+    await user.save();
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+  
+    const token = jwt.sign(data, 'secret_ecom');
+    success = true;
+    res.json({ success, token })
+  })
+  
+  
+
 app.listen(port,(error)=>{
     if(!error){
         console.log(`Server is running on port ${port}`);
